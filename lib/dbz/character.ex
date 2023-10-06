aee2098b09b5928a9a4c532fe48acd3a2181ac4d
Johnson Denen <johnson.denen@gmail.com>
2023-10-06 13:32:31 -0400

    Add code written during session 1

--- lib/dbz/character.ex
+++ lib/dbz/character.ex
@@ -6,4 +6,82 @@ defmodule Dbz.Character do
   - `power_increment`: How much the character's power level grows over time
   """
   use GenServer
+
+  require Logger
+
+  def scan(name) do
+    power_level = GenServer.call(name, :scan)
+    character_name = GenServer.call(name, :character_name)
+
+    "#{character_name}'s power level is #{power_level}"
+  end
+
+  def damage(name, amount) do
+    GenServer.cast(name, {:damage, amount})
+  end
+
+  def kill(name) do
+    GenServer.stop(name, :normal)
+  end
+
+  def start_link(args) do
+    name = Keyword.fetch!(args, :name)
+    GenServer.start_link(__MODULE__, args, name: name)
+  end
+
+  def child_spec(opts) do
+    %{
+      id: Keyword.fetch!(opts, :name),
+      start: {__MODULE__, :start_link, [opts]},
+      restart: :permanent,
+      type: :worker
+    }
+  end
+
+  @impl true
+  def init(args) do
+    [character_name] = args |> Keyword.fetch!(:name) |> Module.split()
+
+    new_state =
+      args
+      |> Enum.into(%{})
+      |> Map.put(:name, character_name)
+
+    {:ok, new_state, {:continue, :setup_growth}}
+  end
+
+  @impl true
+  def handle_continue(:setup_growth, state) do
+    timer = Process.send_after(self(), :grow, 1000)
+    new_state = Map.put(state, :timer, timer)
+
+    {:noreply, new_state}
+  end
+
+  @impl true
+  def handle_info(:grow, state) do
+    new_timer = Process.send_after(self(), :grow, 1000)
+    new_power_level = state.power_level + state.power_increment
+    new_state = %{state | power_level: new_power_level, timer: new_timer}
+
+    {:noreply, new_state}
+  end
+
+  @impl true
+  def handle_call(:scan, _from, state), do: {:reply, state.power_level, state}
+
+  def handle_call(:character_name, _from, state), do: {:reply, state.name, state}
+
+  @impl true
+  def handle_cast({:damage, amount}, state) do
+    new_power_level = state.power_level - amount
+    new_state = Map.put(state, :power_level, new_power_level)
+
+    {:noreply, new_state}
+  end
+
+  @impl true
+  def terminate(_reason, state) do
+    Logger.warn("#{state.name} died with a power level of #{state.power_level}")
+  end
 end
--- test/dbz/character_test.exs
+++ test/dbz/character_test.exs
@@ -1,31 +1,73 @@
 defmodule Dbz.CharacterTest do
   use ExUnit.Case
 
+  import ExUnit.CaptureLog
+
   alias Dbz.Character
 
+  setup do
+    start_supervised!({Character, name: Piccolo, power_level: 100, power_increment: 5})
+    :ok
+  end
+
   describe "start_link/1" do
-    test "names the character"
-    test "works for many names"
-    test "allows the character to grow in power"
+    test "names the character" do
+      assert Process.whereis(Piccolo)
+    end
+
+    test "works for many names" do
+      start_supervised!({Character, name: Gohan, power_level: 10, power_increment: 10})
+      assert Process.whereis(Gohan)
+    end
   end
 
   describe "scan/1" do
-    test "returns the character's power level"
-    test "SCREAMS WHEN THE POWER LEVEL IS OVER 9000!!!"
-    test "accepts a PID argument"
-    test "works for different characters"
+    test "returns the character's power level" do
+      assert Character.scan(Piccolo) == "Piccolo's power level is 100"
+    end
+
+    test "accepts a PID argument" do
+      pid = Process.whereis(Piccolo)
+      assert Character.scan(pid) == "Piccolo's power level is 100"
+    end
   end
 
   describe "damage/2" do
-    test "happens fast"
-    test "reduces the character's power level"
+    test "happens fast" do
+      assert Character.damage(Piccolo, 10) == :ok
+    end
+
+    test "reduces the character's power level" do
+      Character.damage(Piccolo, 10)
+      assert Character.scan(Piccolo) == "Piccolo's power level is 90"
+    end
   end
 
   describe "kill/1" do
-    test "terminates the character"
+    test "terminates the character" do
+      # this isn't the best test demonstrating that the character is terminated
+      # but it does emphasize the fact that the character process state is reset
+      # when supervision respawns it.
+      Character.damage(Piccolo, 50)
+      assert Character.scan(Piccolo) == "Piccolo's power level is 50"
+
+      assert :ok = Character.kill(Piccolo)
+      assert Character.scan(Piccolo) == "Piccolo's power level is 100"
+    end
+
+    test "logs a message on death" do
+      msg = "Piccolo died with a power level of 100"
+      assert capture_log(fn -> Character.kill(Piccolo) end) =~ msg
+    end
   end
 
-  describe "GenServer implementation" do
-    test "characters grow over time"
+  describe "character process" do
+    test "grows in power over time" do
+      assert Character.scan(Piccolo) == "Piccolo's power level is 100"
+      Process.sleep(1001)
+      assert Character.scan(Piccolo) == "Piccolo's power level is 105"
+    end
+
+    test "handles arbitrary messages"
   end
 end
