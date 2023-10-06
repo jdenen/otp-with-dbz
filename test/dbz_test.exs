0013d6bb0b525ae90d08fa5a7f1fb7a557b9ef2c
Johnson Denen <johnson.denen@gmail.com>
2023-10-06 13:33:41 -0400

    Add code written during session 1 (#1)

--- test/dbz_test.exs
+++ test/dbz_test.exs
@@ -1,6 +1,10 @@
 defmodule DbzTest do
   use ExUnit.Case
 
+  describe "main_character/0" do
+    test "returns a living Goku process"
+  end
+
   describe "new_character/3" do
     test "creates a new character"
     test "creates powerful characters who cannot be permanently killed"
